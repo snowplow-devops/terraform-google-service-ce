c14a9d273f5159ab08d18df885ca743d6b945145
jbeemster <joshua.a.beemster@gmail.com>
2023-07-26 09:35:48 +1000

    Add base compute module to use for all Snowplow services (closes #1)

---
+++ main.tf
@@ -0,0 +1,129 @@
+locals {
+  startup_script = templatefile("${path.module}/templates/startup-script.sh.tmpl", {
+    user_supplied_script = var.user_supplied_script
+  })
+}
+
+data "google_compute_image" "ubuntu_20_04" {
+  family  = "ubuntu-2004-lts"
+  project = "ubuntu-os-cloud"
+}
+
+resource "google_compute_instance_template" "tpl" {
+  name_prefix = "${var.name}-"
+  description = "This template is used to create ${var.name} instances"
+
+  instance_description = var.name
+  machine_type         = var.machine_type
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source_image = var.ubuntu_20_04_source_image == "" ? data.google_compute_image.ubuntu_20_04.self_link : var.ubuntu_20_04_source_image
+    auto_delete  = true
+    boot         = true
+    disk_type    = "pd-standard"
+    disk_size_gb = 10
+  }
+
+  # Note: Only one of either network or subnetwork can be supplied
+  #       https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance_template#network_interface
+  network_interface {
+    network    = var.subnetwork == "" ? var.network : ""
+    subnetwork = var.subnetwork
+
+    dynamic "access_config" {
+      for_each = var.associate_public_ip_address ? [1] : []
+
+      content {
+        network_tier = "PREMIUM"
+      }
+    }
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["cloud-platform"]
+  }
+
+  metadata_startup_script = local.startup_script
+
+  metadata = {
+    block-project-ssh-keys = var.ssh_block_project_keys
+
+    ssh-keys = join("\n", [for key in var.ssh_key_pairs : "${key.user_name}:${key.public_key}"])
+  }
+
+  tags = [var.name]
+
+  labels = var.labels
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_health_check" "hc" {
+  count = var.health_check_path != "" ? 1 : 0
+
+  name = var.name
+
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10
+
+  http_health_check {
+    request_path = var.health_check_path
+    port         = var.ingress_port
+  }
+}
+
+resource "google_compute_region_instance_group_manager" "grp" {
+  name = "${var.name}-grp"
+
+  base_instance_name = var.name
+  region             = var.region
+
+  target_size = var.target_size
+
+  dynamic "named_port" {
+    for_each = var.named_port_http != "" ? [1] : []
+
+    content {
+      name = var.named_port_http
+      port = var.ingress_port
+    }
+  }
+
+  version {
+    name              = var.instance_group_version_name
+    instance_template = google_compute_instance_template.tpl.self_link
+  }
+
+  update_policy {
+    type                  = "PROACTIVE"
+    minimal_action        = "REPLACE"
+    max_unavailable_fixed = 3
+  }
+
+  dynamic "auto_healing_policies" {
+    for_each = var.health_check_path != "" ? [1] : []
+
+    content {
+      health_check      = join("", google_compute_health_check.hc.*.self_link)
+      initial_delay_sec = 300
+    }
+  }
+
+  wait_for_instances = true
+
+  timeouts {
+    create = "20m"
+    update = "20m"
+    delete = "30m"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,98 @@
+variable "name" {
+  description = "A name which will be pre-pended to the resources created"
+  type        = string
+}
+
+variable "instance_group_version_name" {
+  description = "A name to give to the instance group version control (e.g. app_name + app_version)"
+  type        = string
+}
+
+variable "region" {
+  description = "The name of the region to deploy within"
+  type        = string
+}
+
+variable "network" {
+  description = "The name of the network to deploy within"
+  type        = string
+}
+
+variable "subnetwork" {
+  description = "The name of the sub-network to deploy within; if populated will override the 'network' setting"
+  type        = string
+  default     = ""
+}
+
+variable "machine_type" {
+  description = "The machine type to use"
+  type        = string
+  default     = "e2-small"
+}
+
+variable "target_size" {
+  description = "The number of servers to deploy"
+  default     = 1
+  type        = number
+}
+
+variable "associate_public_ip_address" {
+  description = "Whether to assign a public ip address to this instance; if false this instance must be behind a Cloud NAT to connect to the internet"
+  type        = bool
+  default     = true
+}
+
+variable "ssh_block_project_keys" {
+  description = "Whether to block project wide SSH keys"
+  type        = bool
+  default     = true
+}
+
+variable "ssh_key_pairs" {
+  description = "The list of SSH key-pairs to add to the servers"
+  default     = []
+  type = list(object({
+    user_name  = string
+    public_key = string
+  }))
+}
+
+variable "named_port_http" {
+  description = "The name to give to the bound port on the instance group"
+  type        = string
+  default     = ""
+}
+
+variable "ingress_port" {
+  description = "The port that the service will be bound to and exposed over HTTP"
+  type        = number
+  default     = -1
+}
+
+variable "health_check_path" {
+  description = "The path to bind for health checks"
+  type        = string
+  default     = ""
+}
+
+variable "user_supplied_script" {
+  description = "The user-data script extension to execute"
+  type        = string
+}
+
+variable "ubuntu_20_04_source_image" {
+  description = "The source image to use which must be based of of Ubuntu 20.04; by default the latest community version is used"
+  default     = ""
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "The name of the service account email address to bind to the deployment"
+  type        = string
+}
+
+variable "labels" {
+  description = "The labels to append to this resource"
+  default     = {}
+  type        = map(string)
+}
